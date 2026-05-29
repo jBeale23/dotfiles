f0b3601ebb09fa17bb4cb0103f9a78a1bcdc6ec9
Josh Beale <jbeale2023@gmail.com>
2026-05-28 20:37:07 -0400

    Added gnome_wallpaper.sh script to automatically change wallpaper via crontab

---
+++ shell/gnome_wallpaper.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# ---------------------------------------------------------------------------------------------- #
+# Basic script to find a wallpaper and set it as the desktop background and screensaver in Gnome #
+# Set to run in crontab every 15 minutes on the hour                                             #
+# ---------------------------------------------------------------------------------------------- #
+
+wallpaper_dir="${HOME}/.config/wallpapers"
+if [[ -d "${wallpaper_dir}" ]]; then
+	wallpaper=$(find "${wallpaper_dir}" -maxdepth 1 -type f -print0 \( -name "*.png" -o -name "*.jpg" \) | shuf -z -n 1 | tr '\0' '\n')
+	gsettings set org.gnome.desktop.background picture-uri-dark "file://${wallpaper}"
+	gsettings set org.gnome.desktop.screensaver picture-uri "file://${wallpaper}"
+fi
